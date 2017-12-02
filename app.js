f872422e0034f48a196c12876a42144180f5a89c
jopo4ka <sobakasobaka6@gmail.com>
2017-12-02 23:12:30 +0500

    change PORT

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var app = require('express')();
 var bot = require('./bot/bot');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', (req, res)=>{
     var bot = require('./bot/bot');
     res.send('<H3>HELLO WORLD</H3>');
 });
-app.listen(3000, ()=>{
-    console.log('app listening port 3000');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
