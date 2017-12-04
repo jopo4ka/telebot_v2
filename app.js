24f5b45d877079d514186968eb6dc89c899e408e
jopo4ka <sobakasobaka6@gmail.com>
2017-12-04 21:31:45 +0500

    add-socket

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
-var dbUtils = require('./database/utils');
-var bot = require('./bot/bot');
+var http = require('http').Server(app);
 var pug = require('pug');
 var path = require('path');
+var io = require('socket.io')(http);
+
+var dbUtils = require('./database/utils');
+var bot = require('./bot/bot');
 
 app.engine('pug', require('pug').__express)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,8 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', (process.env.PORT || 5000));
 app.set('views', './views'); // specify the views directory
 app.set('view engine', 'pug'); // register the template engine
-
-var allUsers;
 
 app.get('/', function (req, res) {
 	dbUtils.getUsers(result=>{
@@ -26,6 +27,15 @@ app.get("/user/:id", function (req, res) {
 		});
 });
 
-app.listen(app.get('port'), function() {
+app.post('/send', (req, res)=>{
+	console.log("Sended text is | "+req.params.text);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
+http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
--- database/utils.js
+++ database/utils.js
@@ -2,8 +2,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var models = require('./models');
 // подключение
-var uri = 'mongodb://jopo4ka:Password1@ds129156.mlab.com:29156/telebot_v2';
-//var uri = 'mongodb://localhost/test'
+//var uri = 'mongodb://jopo4ka:Password1@ds129156.mlab.com:29156/telebot_v2';
+var uri = 'mongodb://localhost/test'
 
 function random(min, max)
 {
@@ -13,12 +13,7 @@ function random(min, max)
 module.exports.addUser = function(msg){
 	mongoose.connect(uri, (err)=>{
 		if (err) throw err;
-		var user = new models.user(
-			{
-			_id : msg.from.id,
-			first_name: msg.from.first_name,
-			last_name: msg.from.last_name
-		});
+		var user = new models.user({_id : msg.from.id, first_name: msg.from.first_name, last_name: msg.from.last_name});
 		user.save((err)=> {
 			mongoose.disconnect();
 			if(err) return console.log(err);
@@ -27,7 +22,7 @@ module.exports.addUser = function(msg){
 	});
 }
 
-module.exports.updCity = function(msg){
+module.exports.updCity = function(msg, imp){
 	mongoose.connect(uri, (err, usr)=>{
 		models.user.findById(msg.from.id, function (err, doc) {
 			if (err) throw err;
@@ -45,13 +40,7 @@ module.exports.addMessage = function(msg, imp){
 	if (imp == undefined) imp = false;
 	mongoose.connect(uri, (err, usr)=>{
 		if (err) throw err;
-		var dbMsg = new models.message(
-			{
-				from: msg.from.id,
-				text: msg.text,
-				important : imp
-			}
-		)
+		var dbMsg = new models.message( { from: msg.from.id, text: msg.text, important : imp } )
 		dbMsg.save((err)=>{
 			mongoose.disconnect();
 			if (err) throw err;
@@ -60,23 +49,24 @@ module.exports.addMessage = function(msg, imp){
 	});
 }
 
-module.exports.addOrder = function(msg, match, group){
+module.exports.addOrder = function(msg, match, group, imp){
 	if (group == undefined) group = "WTF???";
+	if (imp == undefined) imp = false;
 	mongoose.connect(uri, (err, usr)=>{
 		if (err) throw err;
-		var dbOrder = new models.order({
-			owner: msg.from.id,
-			num: random(1000, 9999)|0,
-			text: group +" | "+ match[1]
-		});
+		var dbOrder = new models.order({owner: msg.from.id, num: random(1000, 9999)|0, text: group +" | "+ match[1] });
+		var dbMsg = new models.message({ from: msg.from.id, text: msg.text, important : imp } );
 		dbOrder.save((err)=>{
-			mongoose.disconnect();
 			if(err) throw err;
 			console.log("Object saved: "+ dbOrder)
 		})
+		dbMsg.save((err)=>{
+			mongoose.disconnect();
+			if (err) throw err;
+			console.log('Message succesfull added | text: '+ msg.text);
+		})
 	});
 	console.log('Text message | '+ msg.text)
-	console.log('SK: '+match[1]);
 }
 
 module.exports.getOrders = function(msg, callback){
@@ -95,6 +85,7 @@ module.exports.getUsers = function(callback){
 		if (err) throw err;
 		models.user.find((err, usrs)=>{
 			if (err) throw err;
+			mongoose.disconnect();
 			callback(usrs);
 		});
 	});
